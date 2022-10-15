f8918ca310b3a8513c36a03c5e9c249890d598ed
pavanngre <90178716+pavanngre@users.noreply.github.com>
2022-10-15 15:45:51 +0530

    Update problem2.js

--- Javascript-IV [Conditional Statements ]/problem2.js
+++ Javascript-IV [Conditional Statements ]/problem2.js
@@ -1,1 +1,10 @@
-
+ //If a person is allowed to drive in India print "Apply for a license" or "NA".
+   
+let age=10;
+if(age >= 9){
+  console.log("Apply for license");
+}
+else
+{
+  console.log("NA");n
+}
