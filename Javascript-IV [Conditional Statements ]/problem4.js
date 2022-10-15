3415ce40cab548d58610bc1ca06d68cbe4f24249
pavanngre <90178716+pavanngre@users.noreply.github.com>
2022-10-15 15:49:22 +0530

    Update problem4.js

--- Javascript-IV [Conditional Statements ]/problem4.js
+++ Javascript-IV [Conditional Statements ]/problem4.js
@@ -1,1 +1,22 @@
+//Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let database_user = "xyz@gmail.com";
+let database_pass = "abc@123";
+let user="xyz@gmail.com";
+let pass="123abc";
+if(database_user == user) 
+{
+  if(database_pass == pass){
+    console.log("Login Success");
+  }
+  else
+  {
+    console.log("incorrect password");
+  }
+  
+}
+else {
+  console.log("Wrong");
+}
+
 
