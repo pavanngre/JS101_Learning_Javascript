b1d1aff14cf6f8d4249c0c741fb82b55493e392c
pavanngre <90178716+pavanngre@users.noreply.github.com>
2022-10-15 15:36:09 +0530

    Update problem3.js

--- Introduction to JS (Variables & Data Types)/problem3.js
+++ Introduction to JS (Variables & Data Types)/problem3.js
@@ -1,1 +1,9 @@
-
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+  
+let Name="Pavan";
+var Age=25;
+console.log(Name);
+console.log(typeof(Name));
+console.log(Age);
