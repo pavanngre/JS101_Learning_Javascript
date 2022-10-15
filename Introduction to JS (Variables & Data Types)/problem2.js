cebcdfcebdebcc5afc4ac10762b27bfb4a8b0c18
pavanngre <90178716+pavanngre@users.noreply.github.com>
2022-10-15 15:31:33 +0530

    Update problem2.js

--- Introduction to JS (Variables & Data Types)/problem2.js
+++ Introduction to JS (Variables & Data Types)/problem2.js
@@ -1,1 +1,18 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+  
+
+var Myname;
+Myname="pavan";
+console.log(Myname);
+var Fathername;
+Fathername="Dinkar";
+console.log(Fathername);
+var Motherrname;
+Mothername="Sindhu";
+console.log(Mothername);
 
